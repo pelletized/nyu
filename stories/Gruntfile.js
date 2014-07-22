1670f3ccb6a785b977d189c5fcb7eb228ac82544
Ed Wheeler <ewheeler3@gmail.com>
2014-07-21 19:46:27 -0700

    building nyu stories homepage

---
+++ stories/Gruntfile.js
@@ -0,0 +1,87 @@
+module.exports = function(grunt) {
+	
+	//require('time-grunt')(grunt);
+	
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {   
+			js: {
+				src: [
+					'dev/js/libs/*.js', // All JS in the libs folder
+					'dev/js/nyustories-main-dev.js'  // This specific file
+				],
+				dest: 'dev/js/nyustories-main.js',
+			},
+			css: {
+				src: [
+					'dev/css/*.css'					
+				],
+				dest: 'css/nyustories.css',
+			}			
+		},
+		
+		uglify: {
+			build: {
+				src: 'dev/js/nyustories-main.js',
+				dest: 'js/nyustories-main.min.js'
+			}
+		},
+		
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'images/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'images/'
+				}]
+			}
+		},
+		
+		sass: {
+			dist: {
+				options: {
+					//style: 'compressed',
+					style: 'expanded',
+					spawn: false,
+					
+				},
+				files: {										
+					'dev/css/nyustories.css': 'dev/css/nyustories.scss'
+				}
+			} 
+		},
+		
+		watch: {
+			scripts: {
+				files: ['dev/js/*.js'],
+				tasks: ['concat:js', 'uglify'],
+				options: {
+					spawn: false,
+				},
+			},
+			css: {
+				files: ['dev/css/*.scss'],
+				tasks: ['sass', 'concat:css'],
+				options: {
+					spawn: false,
+				}
+			}
+		}
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['concat', 'uglify']);    
+
+	//5. run imagemin on it's own
+};
---
+++ stories/dev/js/nyustories-main-dev.js
@@ -0,0 +1,93 @@
+//sticky nav
+(function() {
+	var stickyNav = $('.stories-main-nav-wrap').offset().top;	
+	stickyNav = stickyNav - 30; //adjust for smoother transition	
+	$(window).scroll(function(){
+		if( $(window).scrollTop() > stickyNav ) {
+			$('.stories-main-nav-wrap').css({position: 'fixed', top: '25px'});
+		} else {
+			$('.stories-main-nav-wrap').css({position: 'absolute', top: '245px'});
+		}
+	});
+
+})();
+
+//if directly linked to a section, add active class to navbar
+(function() {	
+	if (window.location.hash == "#articles") {
+		$(".nav-articles").addClass("active");				
+	} 
+	
+	else if (window.location.hash == "#videos") {
+		$(".nav-videos").addClass("active");		
+	} 	
+	
+	else if (window.location.hash == "#slideshows") {
+		$(".nav-slideshows").addClass("active");		
+	} 	
+	
+	else {
+		$('.stories-main-nav .active').removeClass('active');
+	}	
+})();
+
+
+//jump links in nav
+$('a[href*=#]:not([href=#])').on("click", function() {	
+	$('.stories-main-nav .active').removeClass('active');
+	$(this).addClass('active');
+
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $(target);
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+        }
+    }	
+});
+
+/*
+$(window).on('hashchange',function(){ 
+	console.log("hash is now: " + document.location.hash);
+	//thisHash = document.location.hash = -1;
+	//console.log(thisHash);
+	//$(".nav-articles").addClass("active");
+});
+*/
+
+//scrolling adds/removes active class on navbar
+(function() {
+var articles = $('#articles').offset().top - 30;
+var videos = $('#videos').offset().top - 50;
+var slideshows = $('#slideshows').offset().top - 50;
+
+	$(window).scroll(function(){		
+		var scroll = $(window).scrollTop();
+				
+		if ((scroll > articles) && (scroll < videos - 50)) {
+			$('.stories-main-nav .active').removeClass('active');
+			$('.stories-main-nav .nav-articles').addClass('active');			
+		} 
+		else if ((scroll > videos) && (scroll < slideshows - 50)) {
+			$('.stories-main-nav .active').removeClass('active');
+			$('.stories-main-nav .nav-videos').addClass('active');
+		}
+		
+		else if (scroll > slideshows) {
+			$('.stories-main-nav .active').removeClass('active');
+			$('.stories-main-nav .nav-slideshows').addClass('active');
+		}
+		
+		else {
+			$('.stories-main-nav .active').removeClass('active');
+		}	
+		
+	});
+
+}) ();
+
+
