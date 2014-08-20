66da27a9dfb14b5c08eee65907b05784755b6f33
Ed Wheeler <ewheeler3@gmail.com>
2014-08-20 16:58:18 -0700

    started working on smallest screen size media query

--- redesign/nav/js/main.js
+++ redesign/nav/js/main.js
@@ -1,23 +1,33 @@
 $(document).ready(function(){
-	toggleTray();	
+	ngnToggleTray();	
 });
 
-function toggleTray() {
-	$("#ngn .ngn-tray").hide();	
+function ngnToggleTray() {
+	// TODO: this should be it's own function
+	$("#ngn .ngn-tray, #ngn #ngnSearch").hide();	
 	
-	$("#ngn .btn-search").on("click", function() {
+	$("#ngn .btn-search").on("click", function(e) {
 		$(this).hide();
-		//$(this).css('opacity','0');
+		
 		$("#ngn .ngn-search-box").animate({
 			"margin-left":"0",
-			"width":"68%"
-			//"width":"toggle"
-		}, 1000);	
+			"width":"68%"			
+		}, 700);
+		$("#ngn #ngnSearch").show();
+		e.preventDefault();
+		
 	});
 	
+	var $width = $(window).width();	
+	console.log("hey hey " + $width);	
 	$("#ngn #tray-toggle").on("click", function() {			
-		$("#ngn .ngn-tray").slideToggle(1000, "linear");	
-		$("#ngn #tray-toggle").toggleClass("btn-more");
-		$("#ngn #tray-toggle").toggleClass("btn-less");	
+		
+		if ($width < 480) {
+			$("#ngn .ngn-tray").slideToggle(1000, "linear");	
+		} else {
+			$("#ngn .ngn-tray").slideToggle(1000, "linear");	
+			$("#ngn #tray-toggle").toggleClass("btn-more");
+			$("#ngn #tray-toggle").toggleClass("btn-less");	
+		}
 	});
 }
